c07dd6fd1e2105109516a299ecb7b84b468dd944
DanGugly <elijahmckoy1997@hotmail.com>
2021-11-22 14:29:24 -0500

    Added button functionality and intent from screen 1 to screen 2

--- app/src/main/java/com/example/notificationkotlin/MainActivity.kt
+++ app/src/main/java/com/example/notificationkotlin/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.notificationkotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.notificationkotlin.databinding.ActivityMain2Binding
+import com.example.notificationkotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.button.setOnClickListener {
+            val intent = Intent(baseContext, MainActivity2::class.java)
+                .apply {
+                    putExtra("DATA", binding.editText.text.toString())
+                    startActivity(this)
+                }
+        }
     }
 }
--- app/src/main/java/com/example/notificationkotlin/MainActivity2.kt
+++ app/src/main/java/com/example/notificationkotlin/MainActivity2.kt
@@ -2,10 +2,16 @@ package com.example.notificationkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("Activity2","Entered activity 2")
     }
 }
