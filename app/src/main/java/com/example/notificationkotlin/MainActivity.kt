f99785c228d40fa7c548c63ef120d288a330ba14
DanGugly <elijahmckoy1997@hotmail.com>
2021-11-22 15:49:45 -0500

    Added notification builder and channel to Activity2

--- app/src/main/java/com/example/notificationkotlin/MainActivity.kt
+++ app/src/main/java/com/example/notificationkotlin/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.notificationkotlin
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.notificationkotlin.databinding.ActivityMain2Binding
@@ -16,6 +20,22 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
+
+
+    /*
+    private fun createNotificationChannel(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val name = "Notification Title"
+            val descriptionText = "Notification Description"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    } */
+
 
     override fun onResume() {
         super.onResume()
--- app/src/main/java/com/example/notificationkotlin/MainActivity2.kt
+++ app/src/main/java/com/example/notificationkotlin/MainActivity2.kt
@@ -1,20 +1,56 @@
 package com.example.notificationkotlin
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationCompat.PRIORITY_DEFAULT
+import androidx.core.app.NotificationManagerCompat
 import com.example.notificationkotlin.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
 
     private lateinit var binding : ActivityMain2Binding
 
+    private val CHANNEL_ID = "channel_id"
+    private val notificationID = 101
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main2)
         binding = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding.root)
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val name = "Notification Title"
+            val descriptionText = "Notification Description"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun sendNotification(){
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("YOUR DATA BELOW:")  //Title of notification
+            .setContentText(binding.textView.text.toString())
+            .setPriority(PRIORITY_DEFAULT)
+
+        with(NotificationManagerCompat.from(this)){
+            notify(notificationID, builder.build())
+        }
     }
 
     override fun onResume() {
@@ -24,6 +60,7 @@ class MainActivity2 : AppCompatActivity() {
 
         binding.button.setOnClickListener{
             Toast.makeText(baseContext,"Sending notification: $text" ,Toast.LENGTH_LONG).show()
+            sendNotification()
         }
     }
 }
