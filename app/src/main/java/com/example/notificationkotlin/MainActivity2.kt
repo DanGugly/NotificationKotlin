a3e9eb80d5c4ddd3122302bcbc659fdb59ec0fee
DanGugly <elijahmckoy1997@hotmail.com>
2021-11-22 14:43:25 -0500

    Received data from intent on screen 2 and added click function to screen 2 button

--- app/src/main/java/com/example/notificationkotlin/MainActivity2.kt
+++ app/src/main/java/com/example/notificationkotlin/MainActivity2.kt
@@ -3,15 +3,27 @@ package com.example.notificationkotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.example.notificationkotlin.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMain2Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        //setContentView(R.layout.activity_main2)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
     override fun onResume() {
         super.onResume()
-        Log.d("Activity2","Entered activity 2")
+        val text:String = intent.getStringExtra("DATA") ?: ""
+        Log.d("Activity2", "Entered activity 2, DATA: $text")
+
+        binding.button.setOnClickListener{
+            Toast.makeText(baseContext,"Sending notification: $text" ,Toast.LENGTH_LONG).show()
+        }
     }
 }
